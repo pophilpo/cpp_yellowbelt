821c501d904d297eb8509585565c1158622ac372
Popov Philipp <philolippo@gmail.com>
2020-11-19 20:23:11 +0300

    Fix int64

--- week_1/avg_temp.cpp
+++ week_1/avg_temp.cpp
@@ -4,35 +4,29 @@
 using namespace std;
 
 int main() {
+  unsigned int size = 0;
+  std::cin >> size;
 
-  size_t size;
-  cin >> size;
-
-  int sum = 0;
-
-  vector<int> records(size);
-
-  for (int &record : records) {
-    cin >> record;
-    sum = sum + record;
+  int64_t sum = 0;
+  std::vector<int> temperatures(size);
+  for (auto &temperature : temperatures) {
+    std::cin >> temperature;
+    sum += temperature;
   }
 
-  int avg = sum / static_cast<int>(size);
-
-  vector<int> result;
-
+  int64_t avg = sum / static_cast<int>(size);
+  std::vector<int> result;
   for (size_t i = 0; i < size; i++) {
-
-    if (records[i] > avg) {
+    if (temperatures[i] > avg) {
       result.push_back(i);
     }
   }
 
-  cout << result.size() << endl;
+  std::cout << result.size() << std::endl;
 
-  for (int &record : result) {
-    cout << record << " ";
+  for (const auto result_index : result) {
+    std::cout << result_index << " ";
   }
-
+  std::cout << std::endl;
   return 0;
 }
