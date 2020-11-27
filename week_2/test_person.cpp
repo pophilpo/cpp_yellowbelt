d1e7484c39b95f1070805f0f840fb9ac7ced05b5
Popov Philipp <philolippo@gmail.com>
2020-11-27 13:48:03 +0300

    Add master solution

--- week_2/test_person.cpp
+++ week_2/test_person.cpp
@@ -12,191 +12,123 @@ using namespace std;
 
 template <class T>
 ostream &operator<<(ostream &os, const vector<T> &s) {
-	os << "{";
-	bool first = true;
-	for (const auto &x : s) {
-		if (!first) {
-			os << ", ";
-		}
-		first = false;
-		os << x;
-	}
-	return os << "}";
+  os << "{";
+  bool first = true;
+  for (const auto &x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
 }
 
 template <class T>
 ostream &operator<<(ostream &os, const set<T> &s) {
-	os << "{";
-	bool first = true;
-	for (const auto &x : s) {
-		if (!first) {
-			os << ", ";
-		}
-		first = false;
-		os << x;
-	}
-	return os << "}";
+  os << "{";
+  bool first = true;
+  for (const auto &x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
 }
 
 template <class K, class V>
 ostream &operator<<(ostream &os, const map<K, V> &m) {
-	os << "{";
-	bool first = true;
-	for (const auto &kv : m) {
-		if (!first) {
-			os << ", ";
-		}
-		first = false;
-		os << kv.first << ": " << kv.second;
-	}
-	return os << "}";
+  os << "{";
+  bool first = true;
+  for (const auto &kv : m) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << kv.first << ": " << kv.second;
+  }
+  return os << "}";
 }
 
 template <class T, class U>
 void AssertEqual(const T &t, const U &u, const string &hint = {}) {
-	if (t != u) {
-		ostringstream os;
-		os << "Assertion failed: " << t << " != " << u;
-		if (!hint.empty()) {
-			os << " hint: " << hint;
-		}
-		throw runtime_error(os.str());
-	}
+  if (t != u) {
+    ostringstream os;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+      os << " hint: " << hint;
+    }
+    throw runtime_error(os.str());
+  }
 }
 
 void Assert(bool b, const string &hint) { AssertEqual(b, true, hint); }
 
 class TestRunner {
 public:
-	template <class TestFunc>
-	void RunTest(TestFunc func, const string &test_name) {
-		try {
-			func();
-			cerr << test_name << " OK" << endl;
-		}
-		catch (exception &e) {
-			++fail_count;
-			cerr << test_name << " fail: " << e.what() << endl;
-		}
-		catch (...) {
-			++fail_count;
-			cerr << "Unknown exception caught" << endl;
-		}
-	}
+  template <class TestFunc>
+  void RunTest(TestFunc func, const string &test_name) {
+    try {
+      func();
+      cerr << test_name << " OK" << endl;
+    } catch (exception &e) {
+      ++fail_count;
+      cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
+    }
+  }
 
-	~TestRunner() {
-		if (fail_count > 0) {
-			cerr << fail_count << " unit tests failed. Terminate" << endl;
-			exit(1);
-		}
-	}
+  ~TestRunner() {
+    if (fail_count > 0) {
+      cerr << fail_count << " unit tests failed. Terminate" << endl;
+      exit(1);
+    }
+  }
 
 private:
-	int fail_count = 0;
+  int fail_count = 0;
 };
 
+void TestPredefinedLastNameFirst() {
+    Person person;
 
-void TestIncognito()
-{
-    Person person_test;
-    person_test.ChangeFirstName(1965, "test_first_name1");
-    person_test.ChangeLastName(1967, "test_last_name1");
-    assert_equal(person_test.GetFullName(1900), "Incognito", "test_1_incog");
+    person.ChangeLastName(1965, "Sergeeva");
+    person.ChangeFirstName(1967, "Polina");
+
+    AssertEqual(person.GetFullName(1964), "Incognito");
+    AssertEqual(person.GetFullName(1966), "Sergeeva with unknown first name");
+    AssertEqual(person.GetFullName(1968), "Polina Sergeeva");
 }
 
-void TestUnknownFirstName()
-{
-    Person person_test;
-    person_test.ChangeLastName(1900, "test_last_name1");
-    person_test.ChangeFirstName(1905, "test_first_name1");
-    assert_equal(person_test.GetFullName(1903), "test_last_name1 with unknown first name", "test_1_unknown_fn");
-}
+void TestPredefined() {
+    Person person;
 
-void TestChangeFirstName()
-{
-    Person person_test;
-    person_test.ChangeFirstName(1900, "test_first_name1");
-    person_test.ChangeFirstName(1902, "test_first_name2");
+    person.ChangeFirstName(1965, "Polina");
+    person.ChangeLastName(1967, "Sergeeva");
 
-    assert_equal(person_test.GetFullName(1899),"Incognito", "test_1");
-    assert_equal(person_test.GetFullName(1900),"test_first_name1 with unknown last name", "test_1_first_name");
-    assert_equal(person_test.GetFullName(1901),"test_first_name1 with unknown last name", "test_1_first_name");
-    assert_equal(person_test.GetFullName(1902),"test_first_name2 with unknown last name", "test_1_first_name");
-}
-void TestUnknownLastName()
-{
-    Person person_test;
-    person_test.ChangeFirstName(1900, "test_first_name1");
-    person_test.ChangeLastName(1905, "test_last_name1");
-    assert_equal(person_test.GetFullName(1903), "test_first_name1 with unknown last name", "test_1_unknown_fn");
-}
-void TestChangeLastName()
-{
-    Person person_test;
-    person_test.ChangeLastName(1900, "test_last_name1");
-    person_test.ChangeLastName(1902, "test_last_name2");
+    AssertEqual(person.GetFullName(1964), "Incognito");
+    AssertEqual(person.GetFullName(1966), "Polina with unknown last name");
+    AssertEqual(person.GetFullName(1968), "Polina Sergeeva");
 
-    assert_equal(person_test.GetFullName(1899), "Incognito", "test_1");
-    assert_equal(person_test.GetFullName(1900), "test_last_name1 with unknown first name", "test_1_last_name");
-    assert_equal(person_test.GetFullName(1901), "test_last_name1 with unknown first name", "test_2_last_name");
-    assert_equal(person_test.GetFullName(1902), "test_last_name2 with unknown first name", "test_3_last_name");
-}
+    person.ChangeFirstName(1969, "Appolinaria");
+    AssertEqual(person.GetFullName(1968), "Polina Sergeeva");
+    AssertEqual(person.GetFullName(1969), "Appolinaria Sergeeva");
+    AssertEqual(person.GetFullName(1970), "Appolinaria Sergeeva");
 
-void TestBothName()
-{
-    Person person_test;
-    person_test.ChangeFirstName(1900, "test_first_name1");
-    person_test.ChangeLastName(1902, "test_last_name1");
+    person.ChangeLastName(1968, "Volkova");
+    AssertEqual(person.GetFullName(1967), "Polina Sergeeva");
+    AssertEqual(person.GetFullName(1968), "Polina Volkova");
+    AssertEqual(person.GetFullName(1969), "Appolinaria Volkova");
 
-    assert_equal(person_test.GetFullName(1903), "test_first_name1 test_last_name1", "test_1_both");
-}
-
-void TestChangeBothName()
-{
-    Person person_test;
-    person_test.ChangeFirstName(1900, "test_first_name1");
-    person_test.ChangeLastName(1902, "test_last_name1");
-    person_test.ChangeFirstName(1904, "test_first_name2");
-    person_test.ChangeLastName(1905, "test_last_name2");
-
-    assert_equal(person_test.GetFullName(1906), "test_first_name2 test_last_name2", "test_1_both");
-}
-
-void SaveFirstName()
-
-{
-    Person person_test;
-    person_test.ChangeFirstName(1900, "test_first_name1");
-    person_test.ChangeLastName(1902, "test_last_name1");
-    person_test.ChangeFirstName(1905, "test_first_name2");
-
-    assert_equal(person_test.GetFullName(1903), "test_first_name1 test_last_name1", "test_1_save");
-}
-
-void SaveLastName()
-{
-    Person person_test;
-    person_test.ChangeLastName(1900, "test_last_name1");
-    person_test.ChangeFirstName(1902, "test_first_name1");
-    person_test.ChangeLastName(1905, "test_last_name2");
-
-    assert_equal(person_test.GetFullName(1903), "test_first_name1 test_last_name1", "test_1_save");
 }
 
 int main() {
-    int main() 
-{
-    test_runner runner;
-    runner.run_test(TestIncognito, "test_incognito");
-    runner.run_test(TestUnknownFirstName, "test_unknown_first_name");
-    runner.run_test(TestChangeFirstName, "test_change_first_name");
-    runner.run_test(TestUnknownLastName, "test_unknown_last_name");
-    runner.run_test(TestChangeLastName, "test_change_last_name");
-    runner.run_test(TestBothName, "test_both_name");
-    runner.run_test(TestChangeBothName, "test_change_both_name");
-    runner.run_test(SaveFirstName, "save_first_name");
-    runner.run_test(SaveLastName, "save_last_name");
+    TestRunner runner;
+    runner.RunTest(TestPredefined, "TestPredefined");
+    runner.RunTest(TestPredefinedLastNameFirst, "TestPredefinedLastNameFirst");
+    return 0;
 }
-
-
-
