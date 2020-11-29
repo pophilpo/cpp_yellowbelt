565ad193bd4d7d160174dec0880d837d9fbaaff5
Popov Philipp <philolippo@gmail.com>
2020-11-29 17:23:49 +0300

    Complete task 1

---
+++ week_3/sum_reverse_sort.cpp
@@ -0,0 +1,15 @@
+#include "sum_reverse_sort.h"
+
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int Sum(int x, int y) { return x + y; }
+
+string Reverse(string s) {
+    reverse(s.begin(), s.end());
+    return s;
+}
+
+void Sort(vector<int>& nums) { sort(nums.begin(), nums.end()); }
