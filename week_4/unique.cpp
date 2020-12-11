9e0bda9bced07611d9499deb1b33e245afb7a767
Popov Philipp <philolippo@gmail.com>
2020-12-11 13:11:45 +0300

    Complete task 4

---
+++ week_4/unique.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+
+
+void RemoveDuplicates(vector<T>& elements)
+{
+    sort(elements.begin(), elements.end());
+    auto last_iter = unique(elements.begin(), elements.end());
+    elements.erase(last_iter, elements.end());
+}
+int main() 
+{
+		vector<int> v1 = { 6, 4, 7, 6, 4, 4, 0, 1 };
+    RemoveDuplicates(v1);
+    for (int x : v1) 
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    vector<string> v2 = { "C", "C++", "C++", "C", "C++" };
+    RemoveDuplicates(v2);
+    for (const string& s : v2)
+    {
+        cout << s << " ";
+    }
+    cout << endl;
+    return 0;
+}
