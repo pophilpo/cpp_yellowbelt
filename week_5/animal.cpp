755c964abc7ca6dbf42ce438e35be1431c3c522a
Popov Philipp <philolippo@gmail.com>
2020-12-16 14:09:44 +0300

    Complete task 1

---
+++ week_5/animal.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+public:
+
+	Animal(const string name): Name(name)
+	{
+
+	}
+	const string Name;
+};
+
+
+class Dog: public Animal {
+public:
+	Dog( const string name="dog" ): Animal(name){
+
+	}
+	void Bark() {
+		cout << Name << " barks: woof!" << endl;
+    }
+};
+
